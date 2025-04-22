beb8063c30848022ca9a4fb18a204204667acd1b
ganeshmurthy <gmurthy@apache.org>
2025-04-22 10:19:23 -0400

    Test multiarch build using docker buildx

--- .github/scripts/image.sh
+++ .github/scripts/image.sh
@@ -19,31 +19,29 @@
 
 set -exo pipefail
 
-CONTAINER=podman
+CONTAINER=docker
 PROJECT_NAME=skupper-router
 CONTAINER_REGISTRY=quay.io
-CONTAINER_ORG=skupper
+#CONTAINER_ORG=skupper
+CONTAINER_ORG=ganeshmurthy
 export BUILDAH_FORMAT=docker
 # If PROJECT_TAG is not defined set PROJECT_TAG to main
 if [ -z "$PROJECT_TAG" ]; then
   PROJECT_TAG=main
 fi
-# PLATFORM can be amd64 or arm64
-if [ -z "$PLATFORM" ]; then
-  PLATFORM=amd64
-fi
-
-PLATFORM_LINUX=linux-${PLATFORM}
-PROJECT_TAG=${PROJECT_TAG}-${PLATFORM_LINUX}
 
 # Building the skupper-router image
 # Pass the VERSION as a build argument so Containerfile can use it when calling compile.sh
 # This version is passed in as a -DVERSION build parameter when building skupper-router.
-${CONTAINER} build --build-arg PLATFORM=$PLATFORM --build-arg VERSION=$VERSION -t ${PROJECT_NAME}:${PROJECT_TAG}  -f ./Containerfile .
+# ${CONTAINER} build --build-arg PLATFORM=$PLATFORM --build-arg VERSION=$VERSION -t ${PROJECT_NAME}:${PROJECT_TAG}  -f ./Containerfile .
 
 # Pushing only when credentials available
 if [[ -n "${CONTAINER_USER}" && -n "${CONTAINER_PASSWORD}" ]]; then
+    ${CONTAINER} buildx build --platform linux/amd64,linux/arm64,linux/s390x,linux/ppc64le --build-arg PLATFORM=$PLATFORM --build-arg VERSION=$VERSION -t ${PROJECT_NAME}:${PROJECT_TAG} -f ./Containerfile .
+
+    # Login to the quay.io container repo.
     ${CONTAINER} login -u ${CONTAINER_USER} -p ${CONTAINER_PASSWORD} ${CONTAINER_REGISTRY}
+
     ${CONTAINER} tag ${PROJECT_NAME}:${PROJECT_TAG} ${CONTAINER_REGISTRY}/${CONTAINER_ORG}/${PROJECT_NAME}:${PROJECT_TAG}
     ${CONTAINER} push ${CONTAINER_REGISTRY}/${CONTAINER_ORG}/${PROJECT_NAME}:${PROJECT_TAG}
 
@@ -60,8 +58,7 @@ if [[ -n "${CONTAINER_USER}" && -n "${CONTAINER_PASSWORD}" ]]; then
     if [ -z "$PUSH_LATEST" ]; then
          echo 'NOT Pushing :latest tag'
     else
-        echo 'Pushing :latest-linux-amd64 tag or :latest-linux-arm64 (image.sh)'
-        PROJECT_TAG_LATEST=latest-${PLATFORM_LINUX}
+        PROJECT_TAG_LATEST=latest
         ${CONTAINER} tag ${PROJECT_NAME}:${PROJECT_TAG} ${CONTAINER_REGISTRY}/${CONTAINER_ORG}/${PROJECT_NAME}:${PROJECT_TAG_LATEST}
         ${CONTAINER} push ${CONTAINER_REGISTRY}/${CONTAINER_ORG}/${PROJECT_NAME}:${PROJECT_TAG_LATEST}
     fi
